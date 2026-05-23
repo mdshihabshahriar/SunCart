061f847b9b2cece3b9e507da8f7772db1c74ac57
mdshihabshahriar <shihabshahriar922@gmail.com>
2026-05-23 23:22:51 +0600

    changes in proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -7,7 +7,7 @@ import { headers } from 'next/headers';
 export async function proxy(request) {
 
     const session = await auth.api.getSession({
-        headers : await headers(),
+        headers : request.headers,
     })
 
     
