83e52b9f93ab3daf559eaccddd352ea8ebca9e3c
mdshihabshahriar <shihabshahriar922@gmail.com>
2026-05-23 21:20:54 +0600

    spinner added

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div className="flex items-center justify-center h-[85vh]">
+            <span className="loading loading-dots loading-xl"></span>
+        </div>
+    );
+};
+
+export default LoadingPage;
